cc4d330483375ef945496e61cdde25664b64d3e0
Kyohei <kurokenken0310@gmail.com>
2016-10-26 00:02:57 +0900

    プレイヤーの攻撃時のエフェクトを追加

---
+++ Assets/Animation/FX/Zangeki/zangeki.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zangeki : MonoBehaviour {
+    private float time;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+
+        if(time >= 0.7f/2)
+        {
+            Color c = GetComponent<SpriteRenderer>().color;
+            c.a -= 0.1f;
+            GetComponent<SpriteRenderer>().color = new Color(c.r, c.g, c.b, c.a);
+
+            if (c.a <= 0)
+                Destroy(this.gameObject);
+        }
+	}
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,15 +6,33 @@ public class Player : MonoBehaviour {
     public AudioClip sore;
     public AudioClip ah;
     public GameObject[] Enemy = new GameObject[3];
+    private float minimum;
+    private float maximum;
+    public float duration = 5.0f;
+    private float startTime;
+
+    public GameObject zangeki;
 
     private int eneCount = 0;
+    private bool isMoving = false;
     // Use this for initialization
     void Start () {
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(isMoving)
+        {
+            float t = (Time.time - startTime) / duration;
+            transform.position = new Vector3(Mathf.SmoothStep(minimum, maximum, t), transform.position.y, 0);
+
+            if (transform.position.x == maximum)
+            {
+                isMoving = false;
+                GetComponent<Animator>().SetBool("isMove", isMoving);
+            }
+        }
 	}
 
     public void Move()
@@ -24,10 +42,15 @@ public class Player : MonoBehaviour {
             GetComponent<AudioSource>().PlayOneShot(ah);
             GetComponent<Animator>().SetTrigger("Damage");
         }
-        else
+        else if(!isMoving)
         {
             GetComponent<AudioSource>().PlayOneShot(yha);
-            transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+            //transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+            isMoving = true;
+            GetComponent<Animator>().SetBool("isMove", isMoving);
+            startTime = Time.time;
+            minimum = transform.position.x;
+            maximum = transform.position.x + 1;
         }
     }
 
@@ -40,5 +63,6 @@ public class Player : MonoBehaviour {
                 eneCount++;
         }
         GetComponent<AudioSource>().PlayOneShot(sore);
+        Instantiate(zangeki, new Vector2(transform.position.x+1,transform.position.y-0.6f), Quaternion.identity);
     }
 }
