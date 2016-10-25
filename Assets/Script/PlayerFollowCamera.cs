766c7451680392c73c1eb7873ac9175afc3ce01c
KyoheiOkawa <kurokenken0310@gmail.com>
2016-10-25 14:13:46 +0900

    シーンmain2追加 カメラでプレイヤーを追いかけるようにした タイミングバーをUIとして表示させるようにした

---
+++ Assets/Script/PlayerFollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollowCamera : MonoBehaviour {
+	public GameObject target;
+
+	private Vector3 startPos;
+	private Vector3 offSet;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		offSet = startPos - target.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position + offSet;
+	}
+}
---
+++ Assets/Script/UITimingBar.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UITimingBar : MonoBehaviour {
+	public long BPM = 120;
+	public float Side; //中心からの距離（左右対称）
+	public GameObject Player;
+
+	private Vector3 startPos;
+	private AudioSource audioSource;//曲を流すオーディオソース
+	private float value; //求めたsinの値を保管しておく
+	// Use this for initialization
+	void Start () {
+		audioSource = GameObject.Find("music").GetComponent<AudioSource>();
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		DesidePosition();
+		int i = Judgment();
+		if(i == 1)
+		{
+			Player.GetComponent<Player>().Move();
+			//Debug.Log("Move");
+		}
+		else if (i == 2)
+		{
+			Player.GetComponent<Player>().Attack();
+			//Debug.Log("Attack");
+		}
+	}
+
+	//use in Update Func
+	private void DesidePosition()
+	{
+		long count = CalBMSCount(audioSource.time) % BMSConstants.BMS_RESOLUTION; //現在のカウント値から1小節のカウント値（０～９６００）に変換
+
+		value = Mathf.Sin((Mathf.PI / 2400) * CalBMSCount(audioSource.time));
+		//transform.position = new Vector2(value * Side, transform.position.y);
+		gameObject.GetComponent<RectTransform>().position = new Vector3(startPos.x + value * Side, transform.position.y,0);
+	}
+
+	private long CalBMSCount(float time)
+	{
+		return (long)(time * ((float)BPM / 60) * (BMSConstants.BMS_RESOLUTION / 4));
+
+	}
+
+	//表拍、裏拍でタイミングよくスペースキーを押せたか判定する関数
+	//戻り値　０失敗　１前進（表）　２攻撃（裏）
+	private int Judgment()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(value <= 0.3f && value >= -0.3f)
+			{
+				return 1;
+			}
+
+			if((value >= 0.9f && value <= 1) || (value <= -0.9f && value >= -1))
+			{
+				return 2;
+			}
+		}
+
+		return 0;
+	}
+}
